af01641643197c1673804b78752f9a567572d64c
francesco-saponaro <francesco.s@zumbat.it>
2026-05-29 09:50:20 +0200

    draft: change the button text to 'FORZA ARSENAL'

--- app/page.tsx
+++ app/page.tsx
@@ -89,7 +89,7 @@ export default function Home() {
                        hover:bg-[#B71C1C] hover:scale-105 active:scale-95"
           >
             <Fuel className="w-6 h-6" />
-            FORZA LAZIOOOOOO0000000000000000000
+            FORZA ARSENAL
           </button>
         </div>
       )}
